d3563c6d2d3795f9464089bc87f581906572be16
LIR-56 <lir-567@rambler.ru>
2021-12-01 20:02:38 +0300

    1.2

--- 1 Sonar Sweep/Main.jl
+++ 1 Sonar Sweep/Main.jl
@@ -13,6 +13,30 @@ function count(filename)
     return result
 end
 
-println(count("example.txt"))
 
+function countByThree(filename)
+    prev = 500000
+    f = open(filename, "r")
+    result = 0
+    a = [0, 0, 0, 0]
+    for i in 2:4
+        a[i] = parse(Int64, readline(f))
+    end
+    for line in readlines(f)
+        a[1] = a[2] #there should be linked list, but i'm too lazy, to bring it here
+        a[2] = a[3]
+        a[3] = a[4]
+        a[4] = parse(Int64, line)
+        if (a[4] > a[1])
+            result += 1
+        end
+    end
+    close(f)
+    return result
+end
+
+println(count("example.txt"))
 println(count("Input1.txt"))
+
+println(countByThree("example.txt"))
+println(countByThree("Input1.txt"))
