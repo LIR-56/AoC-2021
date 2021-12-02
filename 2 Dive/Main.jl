243b90e1d9dd61cb8b10ae6d38594e289f0e7546
LIR-56 <lir-567@rambler.ru>
2021-12-02 08:17:42 +0300

    2.2

---
+++ 2 Dive/Main.jl
@@ -0,0 +1,43 @@
+function calculatePosition(filename)
+    horizontalposition = 0
+    depth = 0
+    f = open(filename, "r")
+    for line in readlines(f)
+        splitted = split(line, " ")
+        n = parse(Int64, splitted[2])
+        if splitted[1] == "forward"
+            horizontalposition += n
+        elseif splitted[1] == "up"
+            depth -= n
+        elseif splitted[1] == "down"
+            depth += n
+        end
+    end
+    return horizontalposition * depth
+end
+
+function calculatePosition2(filename)
+    horizontalposition = 0
+    aim = 0
+    depth = 0
+    f = open(filename, "r")
+    for line in readlines(f)
+        splitted = split(line, " ")
+        n = parse(Int64, splitted[2])
+        if splitted[1] == "forward"
+            horizontalposition += n
+            depth += aim * n
+        elseif splitted[1] == "up"
+            aim -= n
+        elseif splitted[1] == "down"
+            aim += n
+        end
+    end
+    return horizontalposition * depth
+end
+
+println(calculatePosition("example.txt"))
+println(calculatePosition("input1.txt"))
+
+println(calculatePosition2("example.txt"))
+println(calculatePosition2("input1.txt"))
